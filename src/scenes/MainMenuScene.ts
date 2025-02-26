7f663d94f65a81c95ff62e1b5d267d128da0be54
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-02-26 14:46:26 +0100

    Add circular animation to spaceships

--- src/scenes/MainMenuScene.ts
+++ src/scenes/MainMenuScene.ts
@@ -5,8 +5,10 @@ export class MainMenuScene extends Phaser.Scene {
     }
 
     private bg: Phaser.GameObjects.TileSprite;
-    private playerShip: Phaser.GameObjects.Sprite;
-
+    private playerShip1: Phaser.GameObjects.Sprite;
+    private playerShip2: Phaser.GameObjects.Sprite;
+    private playerShip3: Phaser.GameObjects.Sprite;
+    private angle: number = 0;
     preload() {
         this.load.setPath('assets');
 
@@ -19,11 +21,18 @@ export class MainMenuScene extends Phaser.Scene {
     create() {
         this.bg = this.add.tileSprite(0, 0, this.cameras.main.width, this.cameras.main.height, 'bg').setOrigin(0).setTileScale(2);
 
-        const playerShipOffsetX = 64;
-        this.playerShip = this.add.sprite(this.cameras.main.centerX - playerShipOffsetX, this.cameras.main.centerY, 'sprites', 'playerShip1_blue.png');
-        this.playerShip.setAngle(-90);
+        const playerShipOffsetX = 200;
+        this.playerShip1 = this.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY, 'sprites', 'playerShip1_blue.png');
+        this.playerShip1.setAngle(-90);
+
+        this.playerShip2 = this.add.sprite(this.cameras.main.centerX + playerShipOffsetX, this.cameras.main.centerY + 228, 'sprites', 'playerShip2_blue.png');
+        this.playerShip2.setAngle(-90);
+
+        this.playerShip3 = this.add.sprite(this.cameras.main.centerX - playerShipOffsetX, this.cameras.main.centerY + 228, 'sprites', 'playerShip3_blue.png');
+        this.playerShip3.setAngle(-90);
+
         this.anims.create({
-            key: 'playerShipIdle',
+            key: 'playerShipIdle_1',
             frames: [
                 { key: 'sprites', frame: 'playerShip1_blue.png' },
                 { key: 'sprites', frame: 'playerShip1_blue_boost.png' },
@@ -31,19 +40,69 @@ export class MainMenuScene extends Phaser.Scene {
             frameRate: 30,
             repeat: -1,
         });
-
-        this.playerShip.play('playerShipIdle');
-
-        this.tweens.add({
-            targets: this.playerShip,
-            x: this.cameras.main.centerX + playerShipOffsetX,
-            duration: 1000,
-            yoyo: true,
+        this.anims.create({
+            key: 'playerShipIdle_2',
+            frames: [
+                { key: 'sprites', frame: 'playerShip2_blue.png' },
+                { key: 'sprites', frame: 'playerShip2_blue_boost.png' },
+            ],
+            frameRate: 30,
             repeat: -1,
-            ease: 'Quad.easeInOut',
+        });
+        this.anims.create({
+            key: 'playerShipIdle_3',
+            frames: [
+                { key: 'sprites', frame: 'playerShip3_blue.png' },
+                { key: 'sprites', frame: 'playerShip3_blue_boost.png' },
+            ],
+            frameRate: 30,
+            repeat: -1,
         });
 
+        this.playerShip1.play('playerShipIdle_1');
+        this.playerShip2.play('playerShipIdle_2');
+        this.playerShip3.play('playerShipIdle_3');
 
+        const radius = 28; // Rayon du mouvement circulaire
+
+        this.tweens.add({
+            targets: this,
+            angle: 360,
+            duration: 4000,
+            repeat: -1,
+            ease: 'Sine.ease',
+            onUpdate: () => {
+                const radian = Phaser.Math.DegToRad(this.angle);
+                this.playerShip1.x = this.cameras.main.centerX + radius * Math.cos(radian);
+                this.playerShip1.y = this.cameras.main.centerY + radius * Math.sin(radian);
+            }
+        });
+
+        this.tweens.add({
+            targets: this,
+            angle: 360,
+            duration: 3500,
+            repeat: -1,
+            ease: 'Sine.ease',
+            onUpdate: () => {
+                const radian = Phaser.Math.DegToRad(this.angle);
+                this.playerShip2.x = (this.cameras.main.centerX + playerShipOffsetX) + radius * Math.cos(radian);
+                this.playerShip2.y = (this.cameras.main.centerY + 228) + radius * Math.sin(radian);
+            }
+        });
+
+        this.tweens.add({
+            targets: this,
+            angle: 360,
+            duration: 3000,
+            repeat: -1,
+            ease: 'Sine.ease',
+            onUpdate: () => {
+                const radian = Phaser.Math.DegToRad(this.angle);
+                this.playerShip3.x = (this.cameras.main.centerX - playerShipOffsetX) + radius * Math.cos(radian);
+                this.playerShip3.y = (this.cameras.main.centerY + 228) + radius * Math.sin(radian);
+            }
+        });
 
         // COLOR CHANGING
         // this.tweens.add({
