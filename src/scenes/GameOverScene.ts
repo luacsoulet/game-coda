3d17edda3ff89d1f0a02b4dd3b7af3fe4d34e893
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-02-26 09:03:30 +0100

    Adding gameOverScene

--- src/scenes/GameOverScene.ts
+++ src/scenes/GameOverScene.ts
@@ -8,9 +8,9 @@ export class GameOverScene extends Phaser.Scene {
 
     create() {
         this.cameras.main.setBackgroundColor('#000');
+        this.add.text(this.cameras.main.centerX, this.cameras.main.centerY - 800, 'Your score: ' + this.registry.get(GameDataKeys.PlayerScore), { fontSize: '40px', color: '#fff' }).setOrigin(0.5);
         this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, 'Game Over', { fontSize: '96px', color: '#fff' }).setOrigin(0.5);
-        this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 100, 'Your score: ' + this.registry.get(GameDataKeys.PlayerScore), { fontSize: '64px', color: '#fff' }).setOrigin(0.5);
-        this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 200, 'Press SPACE to play again', { fontSize: '64px', color: '#fff' }).setOrigin(0.5);
+        this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 800, 'Press SPACE to play again', { fontSize: '34px', color: '#fff' }).setOrigin(0.5);
 
         this.input.keyboard?.once('keydown-SPACE', () => {
             this.scene.start('MainGameScene');
