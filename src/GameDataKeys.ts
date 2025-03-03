933010ee2cfa8813e39a9be90a40458d2c184f26
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-03-03 05:50:46 +0100

    add management for new info

--- src/GameDataKeys.ts
+++ src/GameDataKeys.ts
@@ -1,4 +1,7 @@
 export class GameDataKeys {
     public static readonly PlayerScore: string = "playerScore";
     public static readonly SelectedShip: string = "selectedShip";
+    public static readonly PlayerShield: string = "playerShield";
+    public static readonly PlayerSpeed: string = "playerSpeed";
+    public static readonly PlayerShootingRate: string = "playerShootingRate";
 }
